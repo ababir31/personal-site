ae661fa4c9335f60c76f48c838093e4178b07250
ababir31 <52133549+ababir31@users.noreply.github.com>
2025-12-28 01:21:13 +0600

    Update and converted to dynamic data using json in SectionAbout.vue

--- ab-port/src/components/SectionAbout.vue
+++ ab-port/src/components/SectionAbout.vue
@@ -1,8 +1,18 @@
 <script setup>
+import aboutData from '@/assets/about-data.json'
+import myImage from '@/assets/images/aboit-me.png'
+
 import IconOnline from '@/components/icons/IconOnline.vue'
 import IconOrganic from '@/components/icons/IconOrganic.vue'
 import IconMarket from '@/components/icons/IconMarket.vue'
 import IconBrand from '@/components/icons/IconBrand.vue'
+
+const iconMap = {
+  IconOnline,
+  IconOrganic,
+  IconMarket,
+  IconBrand,
+}
 </script>
 
 <template>
@@ -19,11 +29,7 @@ import IconBrand from '@/components/icons/IconBrand.vue'
         </div>
 
         <div class="flex items-end gap-6">
-          <img
-            src="E:\FINAL-TRY\dev-portfolio\ab-port\src\assets\images\aboit-me.png"
-            alt=""
-            class="rounded-lg w-[180px] h-auto"
-          />
+          <img :src="myImage" alt="" class="rounded-lg w-[180px] h-auto" />
           <p class="text-white text-[1.125rem]! leading-[1.6em]!">
             There is no template to audit a site or no magic formula make it successful. Every
             project is different. That’s my process takes time but it works.
@@ -31,54 +37,17 @@ import IconBrand from '@/components/icons/IconBrand.vue'
         </div>
       </div>
       <div class="col-start-7 col-span-6 grid grid-cols-2 gap-6">
-        <div class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between">
-          <icon-market />
+        <div
+          v-for="item in aboutData"
+          :key="item.id"
+          class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between"
+        >
+          <component :is="iconMap[item.icon]" />
           <div class="flex flex-col gap-4">
-            <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">Market Placement</h3>
+            <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">{{ item.title }}</h3>
             <p class="text-slate-200 text-[0.833rem]! leading-[1.6em]!">
-              Conduct competitor analysis and market research to position product where it stands
-              out and connects with the right audience.
+              {{ item.description }}
             </p>
-          </div>
-        </div>
-        <div class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between">
-          <icon-brand />
-          <div class="flex flex-col gap-4">
-            <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">Brand Design</h3>
-            <p class="text-slate-200 text-[0.833rem]! leading-[1.6em]!">
-              Design more than a logo, create a full brand impression, from visual identity to the
-              emotional experience your audience feels.
-            </p>
-          </div>
-        </div>
-        <div class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between">
-          <icon-online />
-          <div class="flex flex-col gap-4">
-            <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">Online Presence</h3>
-            <p class="text-slate-200 text-[0.833rem]! leading-[1.6em]!">
-              Build conversion-focused websites and optimize social media profiles to create a
-              consistent, high-impact online footprint
-            </p>
-          </div>
-        </div>
-        <div class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between">
-          <icon-organic />
-          <div class="flex flex-col gap-4">
-            <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">Organic Strategy</h3>
-            <p class="text-slate-200 text-[0.833rem]! leading-[1.6em]!">
-              Use technical SEO, content marketing, and repurposing strategies to drive sustainable,
-              long-term growth without paid ads. This is something to check github.
-            </p>
-          </div>
-          <div class="flex flex-col p-6 border border-blue-500 rounded-lg items-end justify-between">
-            <icon-organic />
-            <div class="flex flex-col gap-4">
-              <h3 class="text-[1.444rem] leading-[1.3em] text-blue-50">Organic Strategy</h3>
-              <p class="text-slate-200 text-[0.833rem]! leading-[1.6em]!">
-                Use technical SEO, content marketing, and repurposing strategies to drive sustainable,
-                long-term growth without paid ads. This is something to check github.
-              </p>
-            </div>
           </div>
         </div>
       </div>
